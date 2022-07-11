060670f86472bbdb71ffe94e9c0392a6edcf84b7
Yaver <vagurin21@gmail.com>
2022-07-11 04:20:09 +0300

    task 7

--- src/vag/lesson7/Airplane.java
+++ src/vag/lesson7/Airplane.java
@@ -1,4 +1,29 @@
 package vag.lesson7;
 
 public class Airplane {
+    private String name;
+    private Wing wing;
+
+    public Airplane(String name, int w) {
+        this.name = name;
+        wing = new Wing();
+        this.wing.setWeight(w);
+    }
+
+    public void airplaneInfo() {
+        System.out.println(this.name);
+        this.wing.airplaneInfo();
+    }
+
+    public class Wing {
+        private int weight;
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+
+        public void airplaneInfo() {
+            System.out.println(" Крыло весит" + this.weight);
+        }
+    }
 }
--- src/vag/lesson7/Main.java
+++ src/vag/lesson7/Main.java
@@ -1,4 +1,10 @@
 package vag.lesson7;
 
 public class Main {
+    public static void main(String[] args) {
+        Airplane airplane1 = new Airplane("Boing", 250);
+        Airplane airplane2 = new Airplane("Airbus", 450);
+        airplane1.airplaneInfo();
+        airplane2.airplaneInfo();
+    }
 }
